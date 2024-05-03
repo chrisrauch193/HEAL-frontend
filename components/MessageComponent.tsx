9ded092713ec23e49e1e480022f26cf864c7b9f0
chrisrauch193 <cr217@st-andrews.ac.uk>
2024-05-03 20:03:51 +0900

    Add styles to messages (#23)

--- components/MessageComponent.tsx
+++ components/MessageComponent.tsx
@@ -1,29 +1,30 @@
 // components/MessageComponent.tsx
 import React from 'react';
 import { View, Text } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import { messagingStyles } from "../styles/messagingStyles";
 import HighlightedText from './HighlightedText';
+import messageStyles from '../styles/messageStyles'; // Ensure the path is correct
 
-interface MessageComponentProps {
-    item: any; // Use your message type
-    current_user_id: string;
-}
-
-const MessageComponent: React.FC<MessageComponentProps> = ({ item, current_user_id }) => {
+const MessageComponent = ({ item, current_user_id }) => {
     const isCurrentUser = item.sender_user_id === current_user_id;
     const messageText = item.content.text; // Default text
-    const medicalTerms = item.content.metadata.medical_terms.map(term => term.name);
+    const medicalTerms = item.content.metadata?.medical_terms?.map(term => term.name) || [];
+
+    // Function to format timestamp using the user's local timezone
+    const formatTime = (timestamp) => {
+        // Using the user's default locale and timezone
+        return new Date(timestamp).toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit', hour12: false });
+    };
 
     return (
-        <View style={isCurrentUser ? messagingStyles.myMessage : messagingStyles.otherMessage}>
-            <Ionicons name="person-circle-outline" size={30} color="black" style={messagingStyles.avatar} />
-            <HighlightedText
-                text={messageText}
-                medicalTerms={medicalTerms}
-                style={messagingStyles.medicalTermHighlight}
-            />
-            <Text style={messagingStyles.timestamp}>{new Date(item.timestamp).toLocaleTimeString()}</Text>
+        <View style={isCurrentUser ? messageStyles.myMessage : messageStyles.otherMessage}>
+            <View style={[messageStyles.messageContent, isCurrentUser ? messageStyles.myMessageContent : messageStyles.otherMessageContent]}>
+                <HighlightedText
+                    text={messageText}
+                    medicalTerms={medicalTerms}
+                    style={messageStyles.medicalTermHighlight}
+                />
+                <Text style={messageStyles.timestamp}>{formatTime(item.timestamp)}</Text>
+            </View>
         </View>
     );
 };
