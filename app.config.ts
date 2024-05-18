b12b881123d6c0f6432626546205946074650a16
chrisrauch193 <cr217@st-andrews.ac.uk>
2024-05-19 05:42:58 +0900

    Add serve (#31)
    * Add serve
    
    * Add eas init
    
    * Add chatbot user
    
    * Fix app.config.ts
    
    * error test
    
    * still not working..
    
    * final fix
    
    * Update pr workflow

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,19 @@
+// app.config.ts
 import 'dotenv/config';
 
 interface AppConfig {
   extra: {
     backendUrl: string;
     useMock: boolean;
+    eas: {
+      projectId: string;
+    };
+  };
+  updates?: {
+    url: string;
+  };
+  runtimeVersion?: {
+    policy: string;
   };
 }
 
@@ -15,6 +25,16 @@ export default ({ config }): AppConfig => {
     extra: {
       backendUrl: isDev ? 'http://13.208.164.92:8888' : 'http://13.208.164.92:8888',
       useMock: isDev,
+      eas: {
+        projectId: 'eba95232-91ba-4791-b166-42dbd7e8803d'
+      }
     },
+    updates: {
+      url: 'https://u.expo.dev/eba95232-91ba-4791-b166-42dbd7e8803d'
+    },
+    runtimeVersion: {
+      policy: 'appVersion'
+    }
   };
 };
+
