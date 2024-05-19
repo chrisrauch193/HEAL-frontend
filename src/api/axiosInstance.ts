5a54858ca8a540488f9049eaf618b5a14d59b669
chrisrauch193 <chrisrauch193@gmail.com>
2024-05-20 06:39:42 +0900

    Add workfows (#43)
    * Add workfows
    
    * Fix workflow
    
    * http redirect (fix later)
    
    * Try again
    
    * Try againgit add .
    
    * Hopefully final one
    
    * Try again

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,18 +2,12 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Constants from 'expo-constants';
-import applyMockAdapter from '@src/mock/server';
 
-const { backendUrl, useMock } = Constants.expoConfig.extra;
+const { backendUrl,  } = Constants.expoConfig.extra;
 
 const axiosInstance = axios.create({
   baseURL: backendUrl,
 });
-
-// if (useMock) {
-if (false) {
-  applyMockAdapter(axiosInstance);
-}
 
 axiosInstance.interceptors.request.use(async (config) => {
   const token = await AsyncStorage.getItem('userToken');
